336ab0f0e69a00b063e3eba4fbf382e2b1046a19
Lucas Costanzo <lcostanzo97@gmail.com>
2024-05-22 01:43:05 +0200

    Adds differential drive control, speed limits and speed normalization

--- motor_control/include/motor_control/motor_control.hpp
+++ motor_control/include/motor_control/motor_control.hpp
@@ -2,6 +2,7 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "motor_control/i2c_device.hpp"
 #include "motor_control/motor.hpp"
@@ -11,19 +12,30 @@
 
 class DiffRobotControlNode : public rclcpp::Node {
     public:
-    DiffRobotControlNode() : Node("diff_control_node") {
-        device_ptr_ = std::make_shared<I2CDevice>();
-        motor_1_ = Motor(device_ptr_, std::make_tuple(8, 9, 10), "1");
-        motor_2_ = Motor(device_ptr_, std::make_tuple(13, 11, 12), "2");
-        subscription_ = this->create_subscription<geometry_msgs::msg::Twist>(
-        "cmd_vel_out", 10, std::bind(&DiffRobotControlNode::cmd_vel_callback, this, std::placeholders::_1));
-    }
+        DiffRobotControlNode() : Node("diff_control_node") {
+            device_ptr_ = std::make_shared<I2CDevice>();
+            motor_1_ = Motor(device_ptr_, std::make_tuple(2, 3, 4), "2");
+            motor_2_ = Motor(device_ptr_, std::make_tuple(8, 10, 9), "1");
+            WHEEL_BASE = 0.1;
+            WHEEL_RADIUS = 0.03;
+            MAX_WHEEL_VELOCITY_RPM = 200.0;
+            MAX_WHEEL_VELOCITY_LINEAR = (MAX_WHEEL_VELOCITY_RPM / 60) * 2 * 3.14 * WHEEL_RADIUS;
+            subscription_ = this->create_subscription<geometry_msgs::msg::Twist>(
+            "cmd_vel_out", 10, std::bind(&DiffRobotControlNode::cmd_vel_callback, this, std::placeholders::_1));
+        }
 
     private:
-    void cmd_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg);
+        void cmd_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg);
+        std::vector<float> cmd_vel_to_motor(geometry_msgs::msg::Twist::SharedPtr velocity);
+        void set_speed_limit(std::vector<float> &speed);
+        float normalize_velocity(float const &velocity, float const &max_velocity);
 
-    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription_;
-    std::shared_ptr<I2CDevice> device_ptr_;
-    Motor motor_1_;
-    Motor motor_2_;
+        rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription_;
+        std::shared_ptr<I2CDevice> device_ptr_;
+        Motor motor_1_;
+        Motor motor_2_;
+        float WHEEL_BASE {};
+        float WHEEL_RADIUS {};
+        float MAX_WHEEL_VELOCITY_RPM {};
+        float MAX_WHEEL_VELOCITY_LINEAR {};
 }; 
--- motor_control/src/motor_control.cpp
+++ motor_control/src/motor_control.cpp
@@ -2,6 +2,10 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
 
 #include "motor_control/i2c_device.hpp"
 #include "motor_control/motor.hpp"
@@ -11,10 +15,42 @@
 #include "std_msgs/msg/string.hpp"
 
 void DiffRobotControlNode::cmd_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg) {
-    float demand_velocity = msg->linear.x;
-    float demand_rotation = msg->angular.z;
+    std::vector<float> motor_velocity = cmd_vel_to_motor(msg);
+    //RCLCPP_INFO(rclcpp::get_logger("rclcpp"), std::to_string(motor_velocity[0]));
+    set_speed_limit(motor_velocity);
+    motor_1_.trySetVelocity(motor_velocity[0]);
+    motor_2_.trySetVelocity(motor_velocity[1]);
+
+    std::cout << "motor 1 velocity: "<< motor_velocity[0] << "    motor 2 velocity: "<< motor_velocity[1] << std::endl;
+    std::cout << "velocity: "<< msg->linear.x << std::endl;
+    std::cout << "max velocity: "<< this->MAX_WHEEL_VELOCITY_LINEAR << std::endl;
+    std::cout << std::endl;
 }
 
+std::vector<float> DiffRobotControlNode::cmd_vel_to_motor(geometry_msgs::msg::Twist::SharedPtr velocity) {
+  
+  std::vector<float> motor_velocity;
+
+  float right_wheel_velocity = (velocity->linear.x + velocity->angular.z * this->WHEEL_BASE / 2.0);  
+  float left_wheel_velocity = (velocity->linear.x - velocity->angular.z * this->WHEEL_BASE / 2.0);
+
+  motor_velocity.push_back(normalize_velocity(right_wheel_velocity, this->MAX_WHEEL_VELOCITY_LINEAR));
+  motor_velocity.push_back(normalize_velocity(left_wheel_velocity, this->MAX_WHEEL_VELOCITY_LINEAR));
+
+  return motor_velocity;
+} 
+
+float DiffRobotControlNode::normalize_velocity(float const &velocity, float const &max_velocity) {
+  return velocity/max_velocity;
+}
+
+void DiffRobotControlNode::set_speed_limit(std::vector<float> &speed) {
+  float scale = std::max(std::abs(speed[0]), std::abs(speed[1]));
+  if (scale > 1) {
+    speed[0] /= scale;
+    speed[1] /= scale;
+  }
+}
 int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<DiffRobotControlNode>());
